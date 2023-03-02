992c9d7d136de80f07d3921b0f774281f0300f28
conrizzo <conrizzo@gmail.com>
2023-03-02 17:16:33 +0100

    fix initial

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,11 @@
 
 const { defineConfig } = require('@vue/cli-service')
 
-module.exports = {
-  publicPath: '/',
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html'
-    }
-  }
-}
 
-//module.exports = {
-//  publicPath: process.env.NODE_ENV === 'production'
-//    ? '/conradswebsite/'
-//    : '/'
-//}
+
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/conradswebsite/'
+    : '/'
+}
