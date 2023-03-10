1e0d4066e673f032e156ba397052ccbcd65db72f
conrizzo <conrizzo@gmail.com>
2023-03-10 17:11:18 +0100

    Cowculator

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,48 +1,46 @@
 <template>
-  <div class="backdrop">
+  <!-- click.self means only this item will activate this function-->
+  <div class="backdrop" @click.self="closeModal">
     <div class="siteMain">
-      <h1>Here is the  title</h1>
-      <h2>{{ header }} </h2>
+      <h1>{{ title }}</h1>
+      <h2>{{ header }}</h2>
       <p>{{ text }}</p>
     </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  props: ['header','text']
-  }
-
+  props: ["title", "header", "text"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
 </script>
 
 
 <!-- scoped applies to only this component -->
 <style scoped>
-    h1{
-     color: rgb(49, 49, 254);
-     border: none;
-     padding: 0;
-    }
+h1 {
+  color: rgb(105, 105, 198);
+  border: none;
+  padding: 0;
+}
 
-
-
-    .siteMain {
-    width: 500px;
-    padding: 25px;
-    margin: 80px auto;
-    background: rgb(255, 255, 255);
-    border-radius: 10px;
-    }
-    .backdrop {
-        top: 0;
-        position: fixed;
-        background: rgba(0,0,0,0.5);
-        width: 100%;
-        height: 100%;
-    }
-    
-
-
+.siteMain {
+  width: 500px;
+  padding: 25px;
+  margin: 80px auto;
+  background: rgb(255, 255, 255);
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
 </style>
---
+++ src/components/UseCalc.vue
@@ -0,0 +1,147 @@
+<template>
+
+<div class="grid-container">
+  <button class="grid-item" @click="addNumber">1</button>
+  <button class="grid-item" @click="addNumberTwo">2</button>
+  <button class="grid-item" @click="addNumberThree">3</button>
+  <button class="grid-item" @click="addNumberFour">4</button>
+  <button class="grid-item" @click="addNumberFive">5</button>
+  <button class="grid-item" @click="addNumberSix">6</button>
+  <button class="grid-item" @click="addNumberSeven">7</button>
+  <button class="grid-item" @click="addNumberEight">8</button>
+  <button class="grid-item" @click="addNumberNine">9</button>
+  <button class="grid-item" @click="addMultiplication">*</button>
+  <button class="grid-item" @click="addNumberZero">0</button>
+  <!-- override styling to fit in more text for the element below -->
+  <button class="grid-item" style="padding:2px;" @click="addMoo">Moo</button>
+
+  
+</div>
+
+  <div>
+    
+  
+    <input
+      style="margin-left: 10px"
+      v-model="displayNumbers"
+      @input="updateNumbers"
+      type="text"
+    />
+    <button class="button-35" style="margin-left: 10px" @click="cowculate">
+      Cowculate
+    </button>
+    <button class="button-35" style="margin-left: 10px" @click="clearField">
+      Reset
+    </button>
+    
+      
+    
+  </div>
+  
+</template>
+<script>
+export default {
+  data() {
+    return {
+      numbers: [],
+      currentOperation: null,
+    };
+  },
+  methods: {
+    addNumber() {
+      this.numbers.push(1);
+    },
+    addNumberTwo() {
+      this.numbers.push(2);
+    },
+    addNumberThree() {
+      this.numbers.push(3);
+    },
+    addNumberFour() {
+      this.numbers.push(4);
+    },
+    addNumberFive() {
+      this.numbers.push(5);
+    },
+    addNumberSix() {
+      this.numbers.push(6);
+    },
+    addNumberSeven() {
+      this.numbers.push(7);
+    },
+    addNumberEight() {
+      this.numbers.push(8);
+    },
+    addNumberNine() {
+      this.numbers.push(9);
+    },
+    addNumberZero() {
+      this.numbers.push(0);
+    },
+    addMoo() {
+      this.numbers.push("Moo");
+    },
+    addMultiplication() {
+      this.currentOperation = "*";
+      this.numbers.push("*");
+    },
+    /* Reset the array - if some error happens or want to restart */
+    clearField() {
+      this.numbers = [];
+    },
+
+    cowculate() {
+      let leftOperand = "";
+      let rightOperand = "";
+      let result = "";
+
+      for (let i = 0; i < this.numbers.length; i++) {
+        if (this.numbers[i] === "*") {
+          /* slice the array positions from the begining to 
+          the multiplication, and right of the multiplication */
+          leftOperand = this.numbers.slice(0, i).join("");
+          rightOperand = this.numbers.slice(i + 1).join("");
+          /* multiply these together */
+          result = Number(leftOperand) * Number(rightOperand);
+          this.numbers = [result];
+          break;
+        }
+      }
+    },
+  },
+  computed: {
+    displayNumbers: {
+      get() {
+        return this.numbers.join("");
+      },
+      set(value) {
+        this.numbers = value.split("");
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  background-color: #fff;
+  padding: 10px;
+  max-width: 250px;
+}
+.grid-item {
+  
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  font-size: 20px;
+  text-align: center;
+  margin: 2px;
+  border-radius: 12px;
+}
+.grid-item:hover{
+  background-color: rgba(186, 186, 186, 0.318);
+}
+
+</style>
