c015dc32c004ebaf350326b61f9484850f6fd23c
conrizzo <conrizzo@gmail.com>
2023-03-10 22:12:14 +0100

    add vue config too

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+//const { defineConfig } = require('@vue/cli-service')
+//module.exports = defineConfig({
+//  transpileDependencies: true
+//})
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/conradswebsite/'
+    : '/'
+}
