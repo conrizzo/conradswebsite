65f6ff8b41828f112a87ba94db77ec65fde28140
conrizzo <conrizzo@gmail.com>
2023-03-12 23:39:56 +0100

    make calculations better

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,63 @@
 
-
 <template>
-  <!--
+  <div style="">
+    <!--
     <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   -->
-  <h1>{{ title }}</h1>
-  <hr style="margin-top: 10px; padding-bottom: 5px;">
-  <p
-    style="
-      display: inline-block;
-      max-width: 500px;
-      text-align: left;
-      padding-bottom: 10px;
-      padding-top: 10px;
-      
-    "
-  >
-  Please enter numbers into the input field below using the keypad or by typing. 
-  The Cow-culator will perform mathematical operations on the numbers and count the number of Moo's! 
-  For instance, if you input Moo2*Moo7, the Cow-culator will display the result as: 'Result: 14 Moo's: 2', which means that there are two 'Moo's and 2 multiplied by 7 equals 14. 
-  Additionally, each input will show up in the array it's used in. Any input field entry or button press will update the array 
-  by pushing the value into it and showing it on the screen.
-  
-  </p>
-  <hr style="padding-top: 5px; padding-bottom: 5px;">
-  <div v-if="showSiteMain">
-    <SiteMain
-      :title="title"
-      :header="header"
-      :text="text"
-      :imagePath="imagePath"
-      :url="url"
-      @close="toggleSiteMain"
-    />
-  </div>
+    <h1>{{ title }}</h1>
+    
+    <p
+      style="
+        display: inline-block;
+        max-width: 500px;
+        text-align: left;
 
-  <UseCalc></UseCalc>
+        padding: 10px;
+      "
+    >
+      Please enter numbers into the input field below using the keypad or by
+      typing. The Cow-culator will perform mathematical operations on the
+      numbers and count the number of Moo's! For instance, if you input
+      Moo2*Moo2*2Moo, the Cow-culator will display the result as: 'Result: 8 Moo's:
+      3', which means that there are three 'Moo's and 2 multiplied by 2 multiplied by 2 equals 8.
+      Additionally, each input will show up in the array it's used in. Any input
+      field entry or button press will update the array by pushing the value
+      into it and showing it on the screen.
+    </p>
+   
+    <div v-if="showSiteMain">
+      <SiteMain
+        :title="title"
+        :header="header"
+        :text="text"
+        :imagePath="imagePath"
+        :url="url"
+        @close="toggleSiteMain"
+      />
+    </div>
 
-  <button
-    class="button-35"
-    style="margin-top: 5px; margin-left: 10px"
-    @click="toggleSiteMain"
-  >
-    About the Cowculator
-  </button>
-  <div>
-    <input style="margin-top: 100px" type="text" ref="name" v-model="name" />
+    <UseCalc></UseCalc>
+
+    <button
+      class="button-35"
+      style="margin-top: 5px; margin-left: 10px"
+      @click="toggleSiteMain"
+    >
+      About the Cowculator
+    </button>
+    <div>
+      <input style="margin-top: 100px" type="text" ref="name" v-model="name" />
+    </div>
+    <button
+      class="button-35"
+      style="margin-top: 20px; margin-left: 10px"
+      @click="handleClick"
+    >
+      Event Listener
+    </button>
   </div>
-  <button
-    class="button-35"
-    style="margin-top: 20px; margin-left: 10px"
-    @click="handleClick"
-  >
-    Event Listener
-  </button>
 </template>
-
 
 
 <script>
@@ -73,7 +74,6 @@ export default {
       title: "Cow-culator built in Vue.js",
       header: "Alpha-Build-1.0",
       text:
-        "Currently not set up to handle more than one multiplication operation. " +
         "Click anywhere on the screen outside this modal to exit! This modal is a component that is an array of 5 props. " +
         "From top to bottom they are props: title, header, text, imagePath, and url. This is a rough approximation of programming languages used to build this: ",
       url: "https://github.com/conrizzo/conradswebsite",
--- src/components/UseCalc.vue
+++ src/components/UseCalc.vue
@@ -1,45 +1,137 @@
 
 
 <template>
-  
   <div class="grid-container cow-image">
-    <button class="grid-item" @click="addNumber(); mooDialogue();">1</button>
-    <button class="grid-item" @click="addNumberTwo(); mooDialogue();">2</button>
-    <button class="grid-item" @click="addNumberThree(); mooDialogue();">3</button>
-    <button class="grid-item" @click="addNumberFour(); mooDialogue();">4</button>
-    <button class="grid-item" @click="addNumberFive(); mooDialogue();">5</button>
-    <button class="grid-item" @click="addNumberSix(); mooDialogue();">6</button>
-    <button class="grid-item" @click="addNumberSeven(); mooDialogue();">7</button>
-    <button class="grid-item" @click="addNumberEight(); mooDialogue();">8</button>
-    <button class="grid-item" @click="addNumberNine(); mooDialogue();">9</button>
-    <button class="grid-item" @click="addMultiplication(); mooDialogue();">*</button>
-    <button class="grid-item" @click="addNumberZero(), mooDialogue();">0</button>
-    
+    <button
+      class="grid-item"
+      @click="
+        addNumber();
+        mooDialogue();
+      "
+    >
+      1
+    </button>
+    <button
+      class="grid-item"
+      @click="
+        addNumberTwo();
+        mooDialogue();
+      "
+    >
+      2
+    </button>
+    <button
+      class="grid-item"
+      @click="
+        addNumberThree();
+        mooDialogue();
+      "
+    >
+      3
+    </button>
+    <button
+      class="grid-item"
+      @click="
+        addNumberFour();
+        mooDialogue();
+      "
+    >
+      4
+    </button>
+    <button
+      class="grid-item"
+      @click="
+        addNumberFive();
+        mooDialogue();
+      "
+    >
+      5
+    </button>
+    <button
+      class="grid-item"
+      @click="
+        addNumberSix();
+        mooDialogue();
+      "
+    >
+      6
+    </button>
+    <button
+      class="grid-item"
+      @click="
+        addNumberSeven();
+        mooDialogue();
+      "
+    >
+      7
+    </button>
+    <button
+      class="grid-item"
+      @click="
+        addNumberEight();
+        mooDialogue();
+      "
+    >
+      8
+    </button>
+    <button
+      class="grid-item"
+      @click="
+        addNumberNine();
+        mooDialogue();
+      "
+    >
+      9
+    </button>
+    <button
+      class="grid-item"
+      @click="
+        addMultiplication();
+        mooDialogue();
+      "
+    >
+      *
+    </button>
+    <button class="grid-item" @click="addNumberZero(), mooDialogue()">0</button>
+
     <!-- override styling to fit in more text for the element below -->
-    <button class="grid-item" style="padding: 2px;" @click="addMoo(); mooDialogue();">Moo</button>
-    
+    <button
+      class="grid-item"
+      style="padding: 2px"
+      @click="
+        addMoo();
+        mooDialogue();
+      "
+    >
+      Moo
+    </button>
   </div>
   <input
-      style="width: 268px; margin-bottom: 7px;"
-      v-model="displayNumbers"
-      @input="updateNumbers, mooDialogue();"
-      type="text"
-    />
-  
-  
+    style="width: 268px; margin-bottom: 7px"
+    v-model="displayNumbers"
+    @input="updateNumbers, mooDialogue()"
+    type="text"
+  />
 
   <div>
-    
-    <button class="button-35"  @click="cowculate">
-      Cowculate
-    </button>
+    <button class="button-35" @click="cowculate">Cowculate</button>
     <button class="button-35" style="margin-left: 5px" @click="clearField">
       Reset
     </button>
-    <!-- Make the Moo button generate random x,y moos on the screen -->
-    <h2  v-if="showText" style="margin-right:20%; font-size: 20px; color: #c4b7a6;" :style="{position: 'absolute', top: textTop + 'px', left: textLeft + 'px' }"> {{ numbers }}</h2>
     
-    
+    <h2
+      v-if="showText"
+      style="
+        padding-top: 5px;
+        padding-bottom: 3px;
+        margin-right: 20%;
+        margin-left: 20%;
+        font-size: 20px;
+        color: #000;
+      "
+    >
+      {{ numbers }}
+    </h2>
   </div>
 </template>
 <script>
@@ -53,7 +145,7 @@ export default {
       buttonY: 0,
     };
   },
-  
+
   methods: {
     addNumber() {
       this.numbers.push(1);
@@ -88,11 +180,9 @@ export default {
     addMoo() {
       this.numbers.push("Moo");
     },
-    mooDialogue(){
+    mooDialogue() {
       this.showText = true;
-      this.textTop = (window.innerHeight-20)
-      this.textLeft = ((window.innerWidth-200)/4)
-      
+
       /* (innerheight / 3.5) will keep it to the top section of the screen area */
       /* this.textTop = Math.random() * (window.innerHeight / 3.5); */
       /* ((window.innerWidth - 75)/1.3) this keeps the text 75 px from the left and makes sure it generates a number
@@ -112,43 +202,29 @@ export default {
     },
 
     cowculate() {
-      let leftOperand = "";
-      let rightOperand = "";
-      let result = "";
-
-      for (let i = 0; i < this.numbers.length; i++) {
-        if (this.numbers[i] === "*") {
-          /* slice the array positions from the begining to 
-          the multiplication, and right of the multiplication */
-          leftOperand = this.numbers.slice(0, i).join("");
-          rightOperand = this.numbers.slice(i + 1).join("");
-          /* Remove the Moo's from the calculator but count them for user using the "Moo" button */
-
-          const countMoo =
-            (leftOperand.match(/Moo/gi) || []).length +
-            (rightOperand.match(/Moo/gi) || []).length; // Count occurrences
-          console.log(countMoo);
-
-          const leftNoMoo = leftOperand.replace(/Moo/gi, "");
-          const rightNoMoo = rightOperand.replace(/Moo/gi, ""); // Remove all occurrences of "Moo"
-
-          /* multiply these together */
-          result = Number(leftNoMoo) * Number(rightNoMoo);
-          if (countMoo > 2) {
-            this.numbers = [
-              "That's a lot of Moo's!" +
-                " Result: " +
-                result +
-                " Moo's: " +
-                countMoo,
-            ];
-          } else {
-            this.numbers = ["Result: " + result + " Moo's: " + countMoo];
-            break;
-          }
-        }
-      }
-    },
+  let moos = 0;
+  let result = 1;
+  
+  for (let i = 0; i < this.numbers.length; i++) {
+    const item = this.numbers[i];
+    
+    if (typeof item === 'number') {
+      result *= item;
+    } else if (item === 'Moo') {
+      moos++;
+    } else if (item === '*') {
+      // do nothing
+    } else {
+      console.log(`Unexpected item in array: ${item}`);
+    }
+  }
+  
+  console.log(`${moos} Moos`);
+  console.log(result);
+  this.numbers = ["Result: " + result + " Moo's: " + moos];
+  return this.numbers;
+}
+    
   },
   computed: {
     displayNumbers: {
@@ -159,7 +235,7 @@ export default {
         this.numbers = value.split("");
       },
     },
-  }
+  },
 };
 </script>
 
