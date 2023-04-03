645a2046bc794d889f4e8f91368bff453e3900c1
conrizzo <conrizzo@gmail.com>
2023-04-03 17:22:14 +0200

    make user typed text automatically activate buttons!

--- src/components/UseCalc.vue
+++ src/components/UseCalc.vue
@@ -2,27 +2,28 @@
 
 <template>
   <div style="padding: 0.15em">
-    <input class="input-field" v-model="expression" type="text" />
+    <input class="input-field" v-model="expression" type="text" @input="checkInput" />
   </div>
   <div class="grid-container cow-image">
-    <button class="grid-item" @click="addNumber()">1</button>
-    <button class="grid-item" @click="addNumberTwo()">2</button>
-    <button class="grid-item" @click="addNumberThree()">3</button>
+    <button class="grid-item" @click="addNumber()" :class="{ active: isActive[1] }">{{ buttonList[1] }}</button>
+    <button class="grid-item" @click="addNumber()" :class="{ active: isActive[2] }">{{ buttonList[2] }}</button>
+    <button class="grid-item" @click="addNumber()" :class="{ active: isActive[3] }">{{ buttonList[3] }}</button>
+
     <button class="grid-item-symbols" @click="addDivision()">÷</button>
 
-    <button class="grid-item" @click="addNumberFour()">4</button>
-    <button class="grid-item" @click="addNumberFive()">5</button>
-    <button class="grid-item" @click="addNumberSix()">6</button>
+    <button class="grid-item" @click="addNumber()" :class="{ active: isActive[4] }">{{ buttonList[4] }}</button>
+    <button class="grid-item" @click="addNumber()" :class="{ active: isActive[5] }">{{ buttonList[5] }}</button>
+    <button class="grid-item" @click="addNumber()" :class="{ active: isActive[6] }">{{ buttonList[6] }}</button>
     <button class="grid-item-symbols" @click="addSubtraction()">-</button>
-    <button class="grid-item" @click="addNumberSeven()">7</button>
-    <button class="grid-item" @click="addNumberEight()">8</button>
-    <button class="grid-item" @click="addNumberNine()">9</button>
+    <button class="grid-item" @click="addNumber()" :class="{ active: isActive[7] }">{{ buttonList[7] }}</button>
+    <button class="grid-item" @click="addNumber()" :class="{ active: isActive[8] }">{{ buttonList[8] }}</button>
+    <button class="grid-item" @click="addNumber()" :class="{ active: isActive[9] }">{{ buttonList[9] }}</button>
     <button class="grid-item-symbols" @click="addMultiplication()">
       &#215;
     </button>
 
     <button class="grid-item" @click="addDecimalPoint()">.</button>
-    <button class="grid-item" @click="addNumberZero()">0</button>
+    <button class="grid-item" @click="addNumber()" :class="{ active: isActive[0] }">{{ buttonList[0] }}</button>
     <button class="grid-item" @click="removeEntry()">
       <div class="arrow-position">
         <div class="left-arrow"></div>
@@ -34,19 +35,15 @@
 
   <div style="margin-bottom: 5px">
     <button class="button-35" @click="clearField">Reset</button>
-    <button
-      class="button-35"
-      style="margin-left: 5px"
-      @click="cowculate(), mooDialogue(), noEntry()"
-    >
+    <button class="button-35" style="margin-left: 5px" @click="cowculate(), mooDialogue(), noEntry()">
       Cowculate
     </button>
   </div>
-  <div style="padding: 0.25em; margin-bottom: 3em">
-    <!-- This code checks for an error message and an empty string to see if user tried to 'cowculate'
+<div style="padding: 0.25em; margin-bottom: 3em">
+  <!-- This code checks for an error message and an empty string to see if user tried to 'cowculate'
       without any input. Then if there is input it pushes the error message to the line below the incorrect input.
-      As long as a correct number math operator sequence is present a correct output is shown.    
-    -->
+        As long as a correct number math operator sequence is present a correct output is shown.    
+      -->
     <h2 class="white-color-text cowculate-result">
       {{ expression }}<span v-if="errorMessage && this.expression == ''"></span>
       <span v-else-if="errorMessage"><br /></span>
@@ -56,8 +53,7 @@
       <h2 class="moo-cows-go-moo">
         <span v-if="mooMessage">
           Moo cows go moo, moo, moo!<br />
-          Moo cows go moo, moo, moo!</span
-        >
+          Moo cows go moo, moo, moo!</span>
       </h2>
     </div>
   </div>
@@ -75,6 +71,10 @@ export default {
       mooMessage: false,
       mooTimer: null,
       errorMessage: false,
+
+      buttonList: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+      isActive: [false, false, false, false, false, false, false, false, false, false]
+
     };
   },
   watch: {
@@ -89,12 +89,50 @@ export default {
     },
   },
   methods: {
+    checkInput() {
+
+
+      let str = this.expression;
+      const lastDigitIndex = str.slice(-1);
+      console.log(lastDigitIndex)
+      if (lastDigitIndex === '0') {
+        this.isActive[0] = true;
+      }
+      else if (lastDigitIndex === '1') {
+        this.isActive[1] = true;
+      } else if (lastDigitIndex === '2') {
+        this.isActive[2] = true;
+      } else if (lastDigitIndex === '3') {
+        this.isActive[3] = true;
+      } else if (lastDigitIndex === '4') {
+        this.isActive[4] = true;
+      } else if (lastDigitIndex === '5') {
+        this.isActive[5] = true;
+      } else if (lastDigitIndex === '6') {
+        this.isActive[6] = true;
+      } else if (lastDigitIndex === '7') {
+        this.isActive[7] = true;
+      } else if (lastDigitIndex === '8') {
+        this.isActive[8] = true;
+      } else if (lastDigitIndex === '9') {
+        this.isActive[9] = true;
+      }
+      setTimeout(() => {
+
+        for (let i = 0; i < 10; i++) {
+          this.isActive[i] = false;
+        }
+
+      }, 100);
+
+
+    },
     cowculate() {
       /* Cow Moo cowculations */
       let str = this.expression;
-      let count = 0;      
+      let count = 0;
       this.moos = count;
-      
+
       str = str.replaceAll("÷", "/").replaceAll("\u00D7", "*").replaceAll("Moo", "");
 
       console.log("Number of 'Moo' occurrences:", count);
@@ -197,6 +235,11 @@ export default {
 </script>
 
 <style scoped>
+button.active {
+  box-shadow: #00ffff 0 0 0 2px;
+  background: rgba(186, 186, 186, 0.318);
+}
+
 .grid-container {
   display: grid;
   grid-template-columns: auto auto auto;
@@ -209,6 +252,7 @@ export default {
   margin-bottom: 10px;
   grid-template-columns: repeat(4, 1fr);
 }
+
 .grid-item {
   background-color: rgba(30, 30, 30, 0.55);
   border: 1px solid rgba(0, 0, 0, 1);
@@ -254,11 +298,13 @@ export default {
   width: 317px;
   font-size: 1.9em;
 }
+
 @media only screen and (max-width: 900px) {
   .moo-cows-go-moo {
     top: 100%;
   }
 }
+
 .cowculate-result {
   padding-top: 1em;
   padding-bottom: 1em;
@@ -295,6 +341,7 @@ export default {
 
   position: absolute;
 }
+
 .arrow-position {
   margin-bottom: 0.8em;
   margin-left: 0.75em;
